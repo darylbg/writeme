c2d045a4ad4384512b869c8ca5fe3b6c3012f875
Daryl Blough <darylxcuf@gmail.com>
2023-10-19 17:12:55 +0100

    added welcome intro text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
+    <div className="app">
+      <section className="welcome-section">
+        <div className="welcome-intro">
+          <h1># My Cool Project</h1>
+          <h2>## Description</h2>
+          <h4>This project is cool and like does cool stuff...</h4>
+        </div>
+        <div className="welcome-text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+      <section className="content-section"></section>
     </div>
   );
 }
