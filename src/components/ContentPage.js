7fc22978a817ef5bb5c79e340d5cf5e7c6b17dea
darylbg <darylxcuf@gmail.com>
2023-10-22 02:14:02 +0100

    styled menu

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import Select from "react-select";
+import { Resizable } from "react-resizable";
 import { FiArrowRight, FiSearch } from "react-icons/fi";
 import axios from "axios";
 
@@ -43,56 +44,61 @@ export default function ContentPage(toggleWelcomeOpen) {
   return (
     <>
       <section className="content-section">
-        <div className="menu">
-          <div className="username-search">
-            <label htmlFor="">Search Github username</label>
-            <div className="error-div">{inputErrorMsg}</div>
-            <input
-              type="text"
-              value={username}
-              placeholder="username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <div className="username-search-icon">
-              <FiSearch onClick={handleGetUserRepos} />
+        <div className="menu-row">
+          <div className="menu">
+            <div className="username-search">
+              <label htmlFor="">Search Github username</label>
+              <div className="error-div">{inputErrorMsg}</div>
+              <input
+                type="text"
+                value={username}
+                placeholder="username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <div className="username-search-icon">
+                <FiSearch onClick={handleGetUserRepos} />
+              </div>
+            </div>
+            <div className="repo-select">
+              <label htmlFor="">Select repository</label>
+              <div className="error-div"></div>
+              <Select
+                options={userRepos}
+                value={selectedRepo}
+                unstyled={true}
+                classNamePrefix="react-select"
+                className="react-select-container"
+                onChange={(selectedOption) => setSelectedRepo(selectedOption)}
+                styles={{
+                  control: (provided, state) => ({
+                    ...provided,
+                    border: state.isFocused ? "1px solid #1b1315" : "none",
+                    borderRadius: state.isFocused ? "4px" : "none",
+                    backgroundColor: state.isSelected ? "red" : "transparent",
+                  }),
+                  option: (provided, state) => ({
+                    ...provided,
+                    backgroundColor: state.isSelected
+                      ? "#720018"
+                      : "transparent",
+                    color: state.isSelected ? "white" : "#1b1315",
+                  }),
+                  placeholder: (provided) => ({
+                    ...provided,
+                    color: "#7c7c7d", // Set your preferred placeholder color
+                  }),
+                }}
+              />
+            </div>
+            <div className="writeme-button">
+              <button>
+                WRITEME
+                <FiArrowRight />
+              </button>
             </div>
           </div>
-          <div className="repo-select">
-            <label htmlFor="">Select repository</label>
-            <div className="error-div"></div>
-            <Select
-              options={userRepos}
-              value={selectedRepo}
-              unstyled={true}
-              classNamePrefix="react-select"
-              className="react-select-container"
-              onChange={(selectedOption) => setSelectedRepo(selectedOption)}
-              styles={{
-                control: (provided, state) => ({
-                  ...provided,
-                  border: state.isFocused ? "1px solid #1b1315" : "none",
-                  borderRadius: state.isFocused ? "4px" : "none",
-                  backgroundColor: state.isSelected ? "red" : "transparent",
-                }),
-                option: (provided, state) => ({
-                  ...provided,
-                  backgroundColor: state.isSelected ? "#720018" : "transparent",
-                  color: state.isSelected ? "white" : "#1b1315",
-                }),
-                placeholder: (provided) => ({
-                  ...provided,
-                  color: "#7c7c7d", // Set your preferred placeholder color
-                }),
-              }}
-            />
-          </div>
-          <div className="writeme-button">
-            <button>
-              WRITEME
-              <FiArrowRight />
-            </button>
-          </div>
         </div>
+        <div className="display-row"></div>
       </section>
     </>
   );
