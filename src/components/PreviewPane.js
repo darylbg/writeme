7a86f57d2aa023a9e4da2f9d8953a308c7fdeac5
darylbg <darylxcuf@gmail.com>
2023-11-08 08:57:57 +0000

    fixed ai api bug in rapid api, fixed markdown lightmode bug

--- src/components/PreviewPane.js
+++ src/components/PreviewPane.js
@@ -1,16 +1,22 @@
 import React from "react";
-import MarkdownPreview from '@uiw/react-markdown-preview';
+import MarkdownPreview from "@uiw/react-markdown-preview";
 
-export default function PreviewPane({markdown}) {
+export default function PreviewPane({ markdown }) {
   return (
     <div className="preview-pane">
       <div className="preview-menu">
         <div className="preview-menu-header">
-          <div className="preview-dot"></div><p>Live preview</p>
+          <div className="preview-dot"></div>
+          <p>Live preview</p>
         </div>
       </div>
       <div className="preview-content">
-        <MarkdownPreview source={markdown} />
+        <MarkdownPreview
+          source={markdown}
+          wrapperElement={{
+            "data-color-mode": "light",
+          }}
+        />
       </div>
     </div>
   );
